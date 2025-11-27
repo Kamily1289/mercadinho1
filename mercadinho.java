a278d62e2fa2365ede22629f676222379d52dffb
Kamily1289 <kamilyguedesmariakamilyguedes@gmail.com>
2025-11-27 13:45:55 -0300

    Add methods to manage and calculate client purchases

--- mercadinho.java
+++ mercadinho.java
@@ -1,3 +1,28 @@
 public class Mercadinho {
+    private Cliente[] Clientes = new Cliente[10];
+    private int pos = 0;
+
+    public void adicionarCliente(Cliente cliente) {
+        Clientes[pos] = cliente;
+        pos++;
+    }
+
+    public double calcularTotal() {
+        double TotalDaCompra = 0;
+        // percorrer a lista
+        for (Cliente each : Clientes) {
+            // pegar os valores e somar
+            TotalDaCompra += each.getValorDaCompra();
+        }
+        return TotalDaCompra;
+        // retornar o total
+    }
+
+    public void imprimirClientes() {
+        for (Cliente cliente : Clientes) {
+            System.out.println(cliente.toString());
+
+        }
+    }
 
 }
